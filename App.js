c0d3881ffc81812d635b8375b8ffcdaf83cb011b
mmunier44 <munier1@gmail.com>
2018-12-10 17:17:15 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,28 @@
+
+import React, { Component } from "react";
+import { AppLoading } from "expo";
+import Navigation from "./components/Navigation";
+
+export default class AnatomyExample extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
+  }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf")
+    });
+    this.setState({ loading: false });
+  }
+
+  render() {
+    if (this.state.loading) {
+      return <AppLoading />;
+    }
+
+    return <Navigation />;
+  }
+}
